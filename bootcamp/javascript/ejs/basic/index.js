fbefaf8dd45f2bfea0f27c5f63c76f5218984458
Anna <24378792+ahjenkins@users.noreply.github.com>
2024-06-13 19:09:50 -0400

    add basic folder

---
+++ bootcamp/javascript/ejs/basic/index.js
@@ -0,0 +1,36 @@
+import express from "express";
+const app = express();
+const port = 3000;
+
+const date = new Date();
+let day = date.getDay();
+let weekday = false;
+let dayText = "";
+let message = "";
+const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+if (day == 0 || day == 7) {
+    weekday = false; 
+} else {
+    weekday = true;
+}
+
+if (weekday) {
+    dayText = "weekday";
+    message = "work hard!";
+} else {
+    dayText = "weekend";
+    message = "have fun!";
+}
+
+app.get("/", (req, res) => {
+    res.render("index.ejs", {
+        "weekday": dayText,
+        "message": message
+    });
+    // console.log(days[day]);
+});
+
+app.listen(port, () => {
+    console.log(`Server listening on ${port}`);
+});
