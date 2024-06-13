9cbdca6395e28e5ffad58690578780822b7415ad
Anna <24378792+ahjenkins@users.noreply.github.com>
2024-06-12 20:18:17 -0400

    update text

--- 2024 web dev bootcamp/jquery/index.js
+++ 2024 web dev bootcamp/jquery/index.js
@@ -1,2 +1,8 @@
 // add multiple classes using a space
 $("h1").addClass("big-title margin-50");
+
+// update text
+// $("h1").text("Bye");
+// $("button").text("Don't click me");
+$("button").html("<em>Hey</em>")
+
