96977c22acde898e013fef8c243bca8c2d0dccaa
Anna <24378792+ahjenkins@users.noreply.github.com>
2024-06-12 20:40:58 -0400

    add website animations

--- 2024 web dev bootcamp/jquery/index.js
+++ 2024 web dev bootcamp/jquery/index.js
@@ -37,3 +37,17 @@ $("h1").prepend("<button>New</button>");
 $("h1").append("<button>New</button>");
 // remove all elements
 // $("button").remove();
+
+// (6) website animations
+$("button").on("click", function() {
+    // $("h1").hide();
+    // $("h1").toggle();
+    // $("h1").fadeOut();
+    // $("h1").fadeIn();
+    // $("h1").fadeToggle();
+    // $("h1").slideUp();
+    // $("h1").slideDown();
+    $("h1").slideToggle();
+    // $("h1").animate({opacity: 0.5}); // numeric values only
+    // $("h1").slideUp().slideDown().animate({opacity: 0.5}); // chain animations together
+})
