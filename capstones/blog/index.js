de6b1893684c6baf11efac9066ce62abe7b385fc
Anna <24378792+ahjenkins@users.noreply.github.com>
2024-06-13 23:15:03 -0400

    render to views

--- capstones/blog/index.js
+++ capstones/blog/index.js
@@ -1,26 +1,29 @@
 import express from "express";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 const app = express();
 const port = 3000;
 
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-    res.send("hello, world");
+    res.render(__dirname + "/views/index.ejs");
 });
 
 app.get("/create", (req, res) => {
-    res.send("create");
-    // res.sendFile("post-create.ejs");
+    res.render("post-create.ejs");
 });
 
 app.get("/update", (req, res) => {
     res.send("update");
-    // res.sendFile("post-update.ejs");
+    // res.render("post-update.ejs");
 });
 
 app.get("/view", (req, res) => {
     res.send("view");
-    // res.sendFile("post-view.ejs");
+    // res.render("post-view.ejs");
 });
 
 app.listen(port, () => {
