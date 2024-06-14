c9edecb94a62cf0cb786de0a49636c55654ae0b4
Anna <24378792+ahjenkins@users.noreply.github.com>
2024-06-13 23:59:55 -0400

    added functionality to submit button

--- capstones/blog/index.js
+++ capstones/blog/index.js
@@ -2,15 +2,16 @@ import express from "express";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 const __dirname = dirname(fileURLToPath(import.meta.url));
+import bodyParser from "body-parser";
 
 const app = express();
 const port = 3000;
 
 let today = new Date();
 let formattedDate = today.toLocaleDateString('en-CA', { year: 'numeric', month: '2-digit', day: '2-digit' });
-console.log(formattedDate);
 
 app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
     res.render(__dirname + "/views/index.ejs");
@@ -20,18 +21,28 @@ app.get("/create", (req, res) => {
     const data = {
         date: formattedDate
     };
-    res.render("post-create.ejs", data);
+    res.render("create.ejs", data);
 });
 
 app.get("/update", (req, res) => {
     res.send("update");
-    // res.render("post-update.ejs");
+    // res.render("update.ejs");
 });
 
 app.get("/view", (req, res) => {
+    // view a post
     res.send("view");
-    // res.render("post-view.ejs");
+    // res.render("view-post.ejs");
 });
+
+app.post("/submit", (req, res) => {
+    const postData = {
+        title: req.body.title,
+        author: req.body.author,
+        content: req.body.content,
+    };
+    res.render("read.ejs", postData);
+})
 
 app.listen(port, () => {
     console.log(`Server listening on port ${port}`);
