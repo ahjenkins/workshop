6b12f3289bbff383c127cedf4f34926bec7409d9
Anna <24378792+ahjenkins@users.noreply.github.com>
2024-06-13 23:02:01 -0400

    set up routes

--- capstones/blog/index.js
+++ capstones/blog/index.js
@@ -6,7 +6,22 @@ app.use(express.static("public"));
 
 app.get("/", (req, res) => {
     res.send("hello, world");
-})
+});
+
+app.get("/create", (req, res) => {
+    res.send("create");
+    // res.sendFile("post-create.ejs");
+});
+
+app.get("/update", (req, res) => {
+    res.send("update");
+    // res.sendFile("post-update.ejs");
+});
+
+app.get("/view", (req, res) => {
+    res.send("view");
+    // res.sendFile("post-view.ejs");
+});
 
 app.listen(port, () => {
     console.log(`Server listening on port ${port}`);
