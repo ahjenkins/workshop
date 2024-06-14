ff79bcb7238ef0fd08230886ddf45c450b9dbbbe
Anna <24378792+ahjenkins@users.noreply.github.com>
2024-06-13 20:12:42 -0400

    add title with length

--- bootcamp/javascript/ejs/templates/index.js
+++ bootcamp/javascript/ejs/templates/index.js
@@ -1,14 +1,26 @@
 import express from "express";
 import bodyParser from "body-parser";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const app = express();
 const port = 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => {});
+app.get("/", (req, res) => {
+  res.render(__dirname + "/views/index.ejs");
+});
 
-app.post("/submit", (req, res) => {});
+app.post("/submit", (req, res) => {
+  const first = req.body.fName;
+  const last = req.body.lName;
+  var length = first.length + last.length;
+  res.render(__dirname + "/views/index.ejs", {
+    length: length
+  })
+});
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
