11352171775d74ca093e7bff0cb9243031ac8e99
Anna <24378792+ahjenkins@users.noreply.github.com>
2024-06-14 22:17:57 -0400

    reorganize code into functions, variables, and arrays

--- capstones/blog/index.js
+++ capstones/blog/index.js
@@ -7,19 +7,28 @@ import bodyParser from "body-parser";
 const app = express();
 const port = 3000;
 
-let today = new Date();
-let formattedDate = today.toLocaleDateString('en-CA', { year: 'numeric', month: '2-digit', day: '2-digit' });
+function dateToday () {
+    let today = new Date();
+    let formattedDate = today.toLocaleDateString('en-CA', { year: 'numeric', month: '2-digit', day: '2-digit' });
+    return formattedDate;
+};
+
+let articleTitle1 = "Fitness Fundamentals: Essential Exercises for Every Level";
 
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
-    res.render(__dirname + "/views/index.ejs");
+    const data = {
+        title: articleTitles,
+        imgThumbnail: imgThumbnails
+    };
+    res.render(__dirname + "/views/index.ejs", data);
 });
 
 app.get("/create", (req, res) => {
     const data = {
-        date: formattedDate
+        date: dateToday()
     };
     res.render("create.ejs", data);
 });
@@ -36,10 +45,15 @@ app.get("/view", (req, res) => {
 });
 
 app.post("/submit", (req, res) => {
+    let title = req.body.title;
+    let author = req.body.author;
+    let content = req.body.content;
+    // articleTitles.push(title);
+
     const postData = {
-        title: req.body.title,
-        author: req.body.author,
-        content: req.body.content,
+        title: title,
+        author: author,
+        content: content,
     };
     res.render("read.ejs", postData);
 })
@@ -47,3 +61,17 @@ app.post("/submit", (req, res) => {
 app.listen(port, () => {
     console.log(`Server listening on port ${port}`);
 });
+
+const articleTitles = [
+    // "Fitness Fundamentals: Essential Exercises for Every Level",
+    // "Achieve Your Fitness Goals: A Step-by-Step Guide",
+    // "Boost Your Energy: Fitness Tips for a Vibrant Lifestyle",
+    // "Nutrition and Fitness: The Perfect Pair for Optimal Health",
+    // "Fitness on the Go: Quick Routines for Busy Schedules",
+    // "The Power of Consistency: Daily Habits for Fitness Success",
+    "Stay Motivated: Tips to Keep Your Fitness Journey on Track"
+];
+
+const imgThumbnails = [
+    "images/fitness1.jpg"
+]
