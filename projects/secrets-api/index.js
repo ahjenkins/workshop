b3c082b3d1e8960f723ae815930d9a5a553f9551
Anna <24378792+ahjenkins@users.noreply.github.com>
2024-07-03 20:27:21 -0400

    use public folder for static files

--- projects/secrets-api/index.js
+++ projects/secrets-api/index.js
@@ -1,13 +1,15 @@
 // HINTS:
 // 1. Import express and axios
 // 2. Create an express app and set the port number.
+// 3. Use the public folder for static files.
 
 import express from "express";
 import axios from "axios";
 
-
 const app = express();
 const port = 3000;
+
+app.use(express.static("public"));
 
 app.get("/", (req, res) => {
     res.send("hello world");
@@ -21,7 +23,7 @@ app.listen(port, () => {
 
 
 
-// 3. Use the public folder for static files.
+
 
 // 4. When the user goes to the home page it should render the index.ejs file.
 
