a94cbb1d083b1769e5a344ff015d668b2d26eb44
Anna <24378792+ahjenkins@users.noreply.github.com>
2024-07-10 15:44:22 -0400

    use findIndex for PUT

--- bootcamp/apis/diy-api/index.js
+++ bootcamp/apis/diy-api/index.js
@@ -49,11 +49,12 @@ app.post("/jokes", (req, res) => {
 //5. PUT a joke
 app.put("/jokes/:id", (req, res) => {
   const id = parseInt(req.params.id);
-  const idIndex = id - 1;
-  console.log(id);
-  console.log("idIndex:", idIndex);
+  // const idIndex = id - 1;
+  const idIndex = jokes.findIndex((joke) => joke.id === id);
   
-  console.log(jokes[idIndex]);
+  // console.log(id);
+  // console.log("idIndex:", idIndex);
+  // console.log(jokes[idIndex]);
   
   // console.log(req.body);
   const text = req.body.text;
@@ -65,8 +66,10 @@ app.put("/jokes/:id", (req, res) => {
     jokeType: type
   }
 
-  console.log(updatedJoke);
-  jokes.splice(idIndex, 1, updatedJoke);
+  // console.log(updatedJoke);
+  // jokes.splice(idIndex, 1, updatedJoke);
+  jokes[idIndex] = updatedJoke;
+  console.log(jokes[idIndex]);
 })
 
 
