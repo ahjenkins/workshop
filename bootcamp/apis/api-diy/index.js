22940c7dcc686e7f36122ecd36789e01ed8ba821
Anna <24378792+ahjenkins@users.noreply.github.com>
2024-07-10 13:54:06 -0400

    create /random route

--- bootcamp/apis/api-diy/index.js
+++ bootcamp/apis/api-diy/index.js
@@ -8,6 +8,14 @@ const masterKey = "4VGP2DN-6EWM4SJ-N6FGRHV-Z3PR3TT";
 app.use(bodyParser.urlencoded({ extended: true }));
 
 //1. GET a random joke
+app.get("/random", async (req, res) => {
+  // console.log(jokes);
+  let randomNum = Math.floor(Math.random() * jokes.length);
+  console.log("num", randomNum);
+  const randomJoke = jokes[randomNum];
+  console.log(randomJoke);
+  res.send("hello");
+})
 
 //2. GET a specific joke
 
