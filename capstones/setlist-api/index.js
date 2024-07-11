63599a02ac1b52eda1884e1ee624927ac3eb3b51
Anna <24378792+ahjenkins@users.noreply.github.com>
2024-07-10 21:34:19 -0400

    add app.use to complete setup

--- capstones/setlist-api/index.js
+++ capstones/setlist-api/index.js
@@ -1,10 +1,16 @@
 import express from 'express';
-import axios from 'axios';
 import bodyParser from 'body-parser';
+import axios from 'axios';
 
 const app = express();
 const port = 3000;
 
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true}));
+
+app.get("/", (req, res) => {
+    res.send("hello world");
+})
 
 app.listen(port, (req, res) => {
     console.log(`Server listening on port ${port}`);
