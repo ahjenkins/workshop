3d0a17e3c7fba0416067fc57aa6e75e1e4ef5fd1
Anna <24378792+ahjenkins@users.noreply.github.com>
2024-07-10 22:43:39 -0400

    add headers for api config

--- capstones/setlist-api/index.js
+++ capstones/setlist-api/index.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import dotenv from 'dotenv';
 import bodyParser from 'body-parser';
 import axios from 'axios';
 
@@ -8,9 +9,19 @@ const port = 3000;
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true}));
 
+const apiKey = process.env.API_KEY;
+const headers = {
+    'Accept': 'application/json',
+    'x-api-key': apiKey,
+    'Accept-Language': 'en'
+}
+
 app.get("/", (req, res) => {
     res.render("index.ejs");
 })
+
+// app.get("/setlist", (req, res) => {
+// })
 
 app.listen(port, (req, res) => {
     console.log(`Server listening on port ${port}`);
