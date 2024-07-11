a36220b157e332aaf88179b13dd422ed0d71942e
Anna <24378792+ahjenkins@users.noreply.github.com>
2024-07-10 23:03:47 -0400

    configure dotenv

--- capstones/setlist-api/index.js
+++ capstones/setlist-api/index.js
@@ -3,6 +3,8 @@ import dotenv from 'dotenv';
 import bodyParser from 'body-parser';
 import axios from 'axios';
 
+dotenv.config();
+
 const app = express();
 const port = 3000;
 
@@ -10,14 +12,24 @@ app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true}));
 
 const apiKey = process.env.API_KEY;
+
 const headers = {
     'Accept': 'application/json',
     'x-api-key': apiKey,
     'Accept-Language': 'en'
 }
 
+// const setlist_API_URL = "https://api.setlist.fm/rest/1.0";
+
 app.get("/", (req, res) => {
     res.render("index.ejs");
+})
+
+app.get("/artist", (req, res) => {
+    console.log("test");
+    // const result = axios.get(setlist_API_URL + "/search/artists?tate", headers);
+    // const result = axios.get("https://api.setlist.fm/rest/1.0/search/artists?artistName=Tate Mcrae&p=1&sort=sortName", headers);
+    // console.log(result.data);
 })
 
 app.post("/search-artist", (req, res) => {
