7eb066ae451e0f34a5f104572b775e17f1b436e9
Anna <24378792+ahjenkins@users.noreply.github.com>
2024-07-15 13:20:58 -0400

    insert addedCountryCode into visited_countries db

--- bootcamp/postgresql/travel-tracker/index.js
+++ bootcamp/postgresql/travel-tracker/index.js
@@ -30,15 +30,17 @@ app.get("/", async (req, res) => {
   // console.log(countries);
   let total = countries.length;
   res.render("index.ejs", {countries, total});
+  // db.end();
 });
 
 app.post("/add", async (req, res) => {
   // console.log("country added");
   const addedCountry = req.body.country;
   const result = await db.query(`SELECT * FROM countries WHERE country_name='${addedCountry}'`);
-  console.log(result.rows);
-  // const result = await db.query("INSERT INTO visited_countries");
-  db.end();
+  const addedCountryCode = result.rows[0].country_code;
+  
+  const data = await db.query("INSERT INTO visited_countries (country_code) VALUES ($1)", [addedCountryCode]);
+  res.redirect("/");
 })
 
 app.listen(port, () => {
