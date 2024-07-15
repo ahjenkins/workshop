85fde94bf1f45b6c3edd529eca4c6b2f6170686d
Anna <24378792+ahjenkins@users.noreply.github.com>
2024-07-15 12:28:50 -0400

    pass visited_countries data to ejs

--- bootcamp/postgresql/travel-tracker/index.js
+++ bootcamp/postgresql/travel-tracker/index.js
@@ -20,10 +20,10 @@ app.use(express.static("public"));
 
 app.get("/", async (req, res) => {
   //Write your code here.
-  let countries = [];
   const result = await db.query("SELECT * FROM visited_countries");
   console.log(result.rows);
   db.end();
+  res.render("index.ejs", {countries: result});
 });
 
 app.listen(port, () => {
